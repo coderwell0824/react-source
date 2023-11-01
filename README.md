bb047c248791002774fe11ab64a365e1234a2dde
coderwell <90953900+coderwell824@users.noreply.github.com>
2023-11-01 21:35:30 +0800

    feat: 触发更新

--- README.md
+++ README.md
@@ -71,3 +71,27 @@ JSX的消费顺序
 归: 对应completeWork
 
 ---
+
+第四课 如何触发更新
+常见的触发更新的方式:
+ReactDOM.createRoot().render(或老版的ReactDOM.render)
+this.setState
+useState的dispatch方法
+
+我们希望实现一套统一的更新机制, 他的特点是:
+兼容上述触发更新的方式
+方便后续扩展(优先级机制...)
+
+更新机制的组成部分
+代表更新的数据结构---Update(shared.pending中可能有多个Update结构)
+消费Update的数据结构 ---UpdateQueue
+
+接下来的工作包括:
+实现mount时调用的API
+将该API接入上述更新机制中
+
+需要考虑的事情:
+更新可能发生于任意组件, 而更新流程时从根节点递归的
+需要一个统一的根节点保存通用信息  --这个就是HostRootFiber
+
+---
--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -1,57 +1,82 @@
 import { beginWork } from "./beginWork";
 import { completeWork } from "./completeWork";
-import { FiberNode } from "./fiber";
+import { FiberNode, FiberRootNode, createWorkInProgress } from "./fiber";
+import { HostRoot } from "./workTags";
 
 let workInProgress: FiberNode | null = null;
 
-function prepareFreshStack(node: FiberNode) {
-	workInProgress = node;
+function prepareFreshStack(root: FiberRootNode) {
+  workInProgress = createWorkInProgress(root.current, {});
 }
 
-function renderRoot(root: FiberNode) {
-	//初始化
-	prepareFreshStack(root);
+export function scheduleUpdateFiber(fiber: FiberNode) {
+  const root = markUpdateFromFiberToRoot(fiber);
+  renderRoot(root);
+}
 
-	do {
-		try {
-			workInProgress();
-			break;
-		} catch (e) {
-			console.error(e, "workLoop 发生错误");
-			workInProgress = null;
-		}
-	} while (true);
+function markUpdateFromFiberToRoot(fiber: FiberNode) {
+
+  let node = fiber;
+  let parent = node.return;
+
+  while (parent != null) {
+    node = parent;
+    parent = node.return;
+  }
+
+  if (node.tag == HostRoot) {
+    return node.stateNode;
+  }
+
+  return null;
+}
+
+
+
+function renderRoot(root: FiberRootNode) {
+  //初始化
+  prepareFreshStack(root);
+
+  do {
+    try {
+      workLoop();
+      break;
+    } catch (e) {
+      console.error(e, "workLoop 发生错误");
+      workInProgress = null;
+    }
+  } while (true);
 }
 
 function workLoop() {
-	while (workInProgress != null) {
-		performUnitOfWork(workInProgress);
-	}
+  while (workInProgress != null) {
+    performUnitOfWork(workInProgress);
+  }
 }
 
 function performUnitOfWork(fiber: FiberNode) {
-	const next = beginWork(fiber);
-	fiber.memoizedProps = fiber.pendingProps;
+  const next = beginWork(fiber);
+  fiber.memoizedProps = fiber.pendingProps;
 
-	if (next === null) {
-		completeUnitOfWork(fiber);
-	} else {
-		workInProgress = next;
-	}
+  if (next === null) {
+    completeUnitOfWork(fiber);
+  } else {
+    workInProgress = next;
+  }
 }
 
 function completeUnitOfWork(fiber: FiberNode) {
-	let node: FiberNode | null = fiber;
+  let node: FiberNode | null = fiber;
 
-	do {
-		completeWork(node);
+  do {
+    completeWork(node);
 
-		const siblings = fiber.sibling;
-		if (siblings !== null) {
-			workInProgress = siblings;
-			return;
-		}
-		node = node.return;
-		workInProgress = node;
-	} while (node !== null);
+    const siblings = fiber.sibling;
+    if (siblings !== null) {
+      workInProgress = siblings;
+      return;
+    }
+    node = node.return;
+    workInProgress = node;
+  } while (node !== null);
 }
